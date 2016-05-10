a8a0833975a15641f9c80e2babffd9d9b95869f5
ernilambar <nilambar@outlook.com>
2016-05-10 16:18:10 +0545

    fix header text issue

--- functions.php
+++ functions.php
@@ -17,9 +17,7 @@ if ( ! function_exists( 'wpnepal_blog_setup' ) ) :
  */
 function wpnepal_blog_setup() {
 
-	/*
-	 * Make theme available for translation.
-	 */
+	// Make theme available for translation.
 	load_theme_textdomain( 'wpnepal-blog' );
 
 	// Add default posts and comments RSS feed links to head.
@@ -79,11 +77,13 @@ function wpnepal_blog_setup() {
 
 	// Set up the WordPress core custom header feature.
     add_theme_support( 'custom-header', apply_filters( 'wpnepal_blog_custom_header_args', array(
-		'default-image' => '',
-		'width'         => 1200,
-		'height'        => 380,
-		'flex-height'   => true,
-		'header-text'   => true,
+		'default-image'      => '',
+		'default-text-color' => '#565656',
+		'width'              => 1200,
+		'height'             => 380,
+		'flex-height'        => true,
+		'header-text'        => true,
+		'wp-head-callback'   => 'wpnepal_blog_header_style',
     ) ) );
 
     /*
@@ -138,22 +138,25 @@ add_action( 'widgets_init', 'wpnepal_blog_widgets_init' );
  * Enqueue scripts and styles.
  */
 function wpnepal_blog_scripts() {
+
 	$min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+
     wp_enqueue_style( 'wpnepal-blog-google-fonts', wpnepal_blog_fonts_url(), array(), null );
 
     wp_enqueue_style( 'genericons', get_template_directory_uri() . '/third-party/genericons/genericons' . $min . '.css', array(), '3.4.1' );
 
-	wp_enqueue_style( 'wpnepal-blog-style', get_stylesheet_uri(), array(), '1.0.0' );
+	wp_enqueue_style( 'wpnepal-blog-style', get_stylesheet_uri(), array(), '1.1' );
 
 	wp_enqueue_script( 'wpnepal-blog-navigation', get_template_directory_uri() . '/js/navigation' . $min . '.js', array(), '20151215', true );
 
     wp_enqueue_script( 'wpnepal-blog-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix' . $min . '.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'wpnepal-blog-custom', get_template_directory_uri() . '/js/custom' . $min . '.js', array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'wpnepal-blog-custom', get_template_directory_uri() . '/js/custom' . $min . '.js', array( 'jquery' ), '1.1', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
 }
 add_action( 'wp_enqueue_scripts', 'wpnepal_blog_scripts' );
 
