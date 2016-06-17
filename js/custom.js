7acc95faf622eb3fcc6ac18335e5e54aa18a44f3
ernilambar <nilambar@outlook.com>
2016-06-17 14:19:26 +0545

    Refactor mobile menu

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,5 @@
+/* global WPNepalBlogScreenReaderText */
+
 ( function( $ ) {
 
 	'use strict';
@@ -21,111 +23,32 @@
 				return false;
 			});
 		}
+
+		// Mobile menu.
+	    $( '#menu-toggle' ).click( function() {
+	        $( '#site-header-menu' ).toggle( 'slow' );
+		});
+
+	  	function initMainNavigation( container ) {
+		    // Add dropdown toggle that display child menu items.
+		    container.find( '.menu-item-has-children > a' ).after( '<button class="dropdown-toggle" aria-expanded="false">' + WPNepalBlogScreenReaderText.expand + '</button>' );
+
+		    // Toggle buttons and submenu items with active children menu items.
+		    container.find( '.current-menu-ancestor > button' ).addClass( 'toggle-on' );
+		    container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
+
+		    container.find( '.dropdown-toggle' ).click( function( e ) {
+		    	var _this = $( this );
+		    	e.preventDefault();
+		    	_this.toggleClass( 'toggle-on' );
+		    	_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
+		    	_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+		    	_this.html( _this.html() === WPNepalBlogScreenReaderText.expand ? WPNepalBlogScreenReaderText.collapse : WPNepalBlogScreenReaderText.expand );
+		    } );
+		}
+
+		// Trigger menu.
+		initMainNavigation( $( '.main-navigation' ) );
+
 	});
-} )( jQuery );
-
-/* global WPNepalBlogScreenReaderText */
-
-/**
- * Theme functions file.
- *
- * Contains handlers for navigation and widget area.
- */
-
-( function( $ ) {
-	var masthead, menuToggle, siteNavigation, siteHeaderMenu;
-
-	function initMainNavigation( container ) {
-
-		// Add dropdown toggle that displays child menu items.
-		var dropdownToggle = $( '<button />', {
-			'class': 'dropdown-toggle',
-			'aria-expanded': false
-		} ).append( $( '<span />', {
-			'class': 'screen-reader-text',
-			text: WPNepalBlogScreenReaderText.expand
-		} ) );
-
-		container.find( '.menu-item-has-children > a' ).after( dropdownToggle );
-
-		// Toggle buttons and submenu items with active children menu items.
-		container.find( '.current-menu-ancestor > button' ).addClass( 'toggled-on' );
-		container.find( '.current-menu-ancestor > .sub-menu' ).addClass( 'toggled-on' );
-
-		// Add menu items with submenus to aria-haspopup="true".
-		container.find( '.menu-item-has-children' ).attr( 'aria-haspopup', 'true' );
-
-		container.find( '.dropdown-toggle' ).click( function( e ) {
-			var _this            = $( this ),
-				screenReaderSpan = _this.find( '.screen-reader-text' );
-
-			e.preventDefault();
-			_this.toggleClass( 'toggled-on' );
-			_this.next( '.children, .sub-menu' ).toggleClass( 'toggled-on' );
-
-			// jscs:disable
-			_this.attr( 'aria-expanded', _this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
-			// jscs:enable
-			screenReaderSpan.text( screenReaderSpan.text() === WPNepalBlogScreenReaderText.expand ? WPNepalBlogScreenReaderText.collapse : WPNepalBlogScreenReaderText.expand );
-		} );
-	}
-	initMainNavigation( $( '.main-navigation' ) );
-
-	masthead         = $( '#masthead' );
-	menuToggle       = masthead.find( '#menu-toggle' );
-	siteHeaderMenu   = masthead.find( '#site-header-menu' );
-	siteNavigation   = masthead.find( '#site-navigation' );
-
-	// Enable menuToggle.
-	( function() {
-
-		// Return early if menuToggle is missing.
-		if ( ! menuToggle.length ) {
-			return;
-		}
-
-		// Add an initial values for the attribute.
-
-		menuToggle.on( 'click.twentysixteen', function() {
-			$( this ).add( siteHeaderMenu ).toggleClass( 'toggled-on' );
-		} );
-	} )();
-
-	// Fix sub-menus for touch devices and better focus for hidden submenu items for accessibility.
-	( function() {
-		if ( ! siteNavigation.length || ! siteNavigation.children().length ) {
-			return;
-		}
-
-		// Toggle `focus` class to allow submenu access on tablets.
-		function toggleFocusClassTouchScreen() {
-			if ( window.innerWidth >= 910 ) {
-				$( document.body ).on( 'touchstart.twentysixteen', function( e ) {
-					if ( ! $( e.target ).closest( '.main-navigation li' ).length ) {
-						$( '.main-navigation li' ).removeClass( 'focus' );
-					}
-				} );
-				siteNavigation.find( '.menu-item-has-children > a' ).on( 'touchstart.twentysixteen', function( e ) {
-					var el = $( this ).parent( 'li' );
-
-					if ( ! el.hasClass( 'focus' ) ) {
-						e.preventDefault();
-						el.toggleClass( 'focus' );
-						el.siblings( '.focus' ).removeClass( 'focus' );
-					}
-				} );
-			} else {
-				siteNavigation.find( '.menu-item-has-children > a' ).unbind( 'touchstart.twentysixteen' );
-			}
-		}
-
-		if ( 'ontouchstart' in window ) {
-			$( window ).on( 'resize.twentysixteen', toggleFocusClassTouchScreen );
-			toggleFocusClassTouchScreen();
-		}
-
-		siteNavigation.find( 'a' ).on( 'focus.twentysixteen blur.twentysixteen', function() {
-			$( this ).parents( '.menu-item' ).toggleClass( 'focus' );
-		} );
-	} )();
 } )( jQuery );
