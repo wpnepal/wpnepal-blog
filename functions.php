22dc96d65c974db2898c920f7760e168e61b696e
ernilambar <nilambar@outlook.com>
2016-09-11 09:56:48 +0545

    Add description for sidebar

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@ function wpnepal_blog_widgets_init() {
 	register_sidebar( array(
 		'name'          => esc_html__( 'Sidebar', 'wpnepal-blog' ),
 		'id'            => 'sidebar-1',
-		'description'   => '',
+		'description'   => __( 'Add widgets here to appear in your sidebar.', 'wpnepal-blog' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
